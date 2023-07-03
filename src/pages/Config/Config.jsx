536d7cade2dd67bfbd262628679c95e8d926e8a2
Rônero <ronerojr@gmail.com>
2023-07-03 09:10:18 -0300

    adc config e sidebar

--- src/pages/Config/Config.jsx
+++ src/pages/Config/Config.jsx
@@ -1,30 +1,30 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// import Sidebar from "../components/SideBar/SideBar"
-// // import Header from "../components/Header/Header"
-// import './Config.css'
+import Sidebar from "../components/SideBar/SideBar"
+// import Header from "../components/Header/Header"
+import './Config.css'
 
-// export function Config() {
+export function Config() {
 
-//     const [cor, setValor] = useState('#000');
+    const [cor, setValor] = useState('#000');
 
-//     const pegarCor = (event) => {
-//         setValor(event.target.value);
-//         document.querySelector('body').style.backgroundColor = cor
-//         console.log(cor)
-//     }; 
+    const pegarCor = (event) => {
+        setValor(event.target.value);
+        document.querySelector('body').style.backgroundColor = cor
+        console.log(cor)
+    }; 
 
     
-//     return (
-//         <>
-//         <div className="page-conteudo"> 
-//             <h1>CONFIGURAÇÕES</h1>
-//             <div>
-//             <input type="color" id="cor" name="cor" value={cor} onChange={pegarCor} />
+    return (
+        <>
+        <div className="page-conteudo"> 
+            <h1>CONFIGURAÇÕES</h1>
+            <div>
+            <input type="color" id="cor" name="cor" value={cor} onChange={pegarCor} />
             
-//                 <label for="cor">Head</label>
-//             </div>
-//         </div>
-//         </>
-//     )
-// }
+                <label for="cor">Head</label>
+            </div>
+        </div>
+        </>
+    )
+}
