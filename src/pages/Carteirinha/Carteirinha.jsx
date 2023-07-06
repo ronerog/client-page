987f1ce07df8a7044527c57b8c586ae58b0a2b2b
Rônero <ronerojr@gmail.com>
2023-07-06 03:15:34 -0300

    botao zap flutante

--- src/pages/Carteirinha/Carteirinha.jsx
+++ src/pages/Carteirinha/Carteirinha.jsx
@@ -16,7 +16,7 @@ export function Carteirinha() {
     useEffect(() => {
 
         async function loadUser(){
-            const request = await fetch('http://jiapi-wpp.vps-kinghost.net:3003/searchmat?DataBaseName=sigef_web_teste&MAT=11000')
+            const request = await fetch('http://jiapi-wpp.vps-kinghost.net:3003/searchmat?DataBaseName=sigef_teste&MAT=11000')
             const result = await request.json();
             const planos = result?.map((plano) => plano.PLA_NOME)
             setData(result);
